e13fcad1ae12d383804491305d686f2c697c3468
Arc4nus <agustin.procichiani@gmail.com>
2023-02-28 15:37:19 -0300

    primer commit

---
+++ CV/JS/function.js
@@ -0,0 +1,27 @@
+function MostrarTexto1() {
+    document.getElementById("Datos").innerHTML = "Datos Personales"
+}
+
+function MostrarTexto2() {
+    document.getElementById("Formacion").innerHTML = "Formación"
+}
+
+function MostrarTexto3() {
+    document.getElementById("Experiencia").innerHTML = "Experiencia"
+}
+
+function MostrarTexto4() {
+    document.getElementById("Habilidades").innerHTML = "Habilidades"
+}
+
+function MostrarTexto5() {
+    document.getElementById("Idiomas").innerHTML = "Idiomas"
+}  
+    
+function OcultarTexto() {
+    document.getElementById("Datos").innerHTML = ""
+    document.getElementById("Formacion").innerHTML = ""
+    document.getElementById("Experiencia").innerHTML = ""
+    document.getElementById("Habilidades").innerHTML = ""
+    document.getElementById("Idiomas").innerHTML = ""
+  }
